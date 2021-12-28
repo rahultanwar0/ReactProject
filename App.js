81e87f4d3f3705d1abd7721ff9970809589d916a
RAHUL TANWAR <rahul@magicapps.co.in>
2021-12-28 15:34:31 +0530

    input form

--- App.js
+++ App.js
@@ -1,33 +1,71 @@
- 
 import React from 'react';
- 
-import {
-  Text, 
-  View,
-} from 'react-native';
+
+import {Text, View, TextInput, Button,StyleSheet} from 'react-native';
 import Login from './component/home';
 
- 
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+      password: '',
+      address: '',
+    };
+  }
 
-const App  = () => {
+  sumbit() {
+    alert(this.state.name+this.state.password+this.state.address);
+  }
 
-
-  const data  = 12
+  render() {
     return (
-     <View>
+      <View>
+        <TextInput
+          placeholder="Enter Name"
+          style={styles.textBox}
+          onChangeText={text => {
+            this.setState({name: text});
+          }}></TextInput>
+        <TextInput
+          placeholder="Enter Password"
+          style={styles.textBox}
+          secureTextEntry={true}
 
-       <Login data={data}/>
+          onChangeText={text => {
+            this.setState({password: text});
+          }}></TextInput>
+        <TextInput
+          placeholder="Enter Address"
+          style={styles.textBox}
+
+          onChangeText={text => {
+            this.setState({address: text});
+          }}></TextInput>
+
+        <Button
  
 
-       <Text>
+          title="Login"
+          onPress={() => {
+            this.sumbit();
+          }}
+        />
+      </View>
+    );
+  }
+}
 
-         hellow
 
-       </Text>
-     </View>
-  );
-};
+const styles = StyleSheet.create({
+  textBox: {
+    borderColor: 'skyblue',
+    borderWidth: 2,
+    padding: 10,
+    marginHorizontal: 20,
+    marginVertical: 25,
+  },
+});
 
- 
 
 export default App;
+
